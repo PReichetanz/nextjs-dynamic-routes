ffcb305e4891076d32abf2673e392fa3a1c0da84
PReichetanz <paul.reichetanz@gmx.de>
2023-03-09 14:58:05 +0100

    initial commit

--- pages/movies/index.js
+++ pages/movies/index.js
@@ -1,13 +1,20 @@
 import Link from "next/link";
+import { movies } from "../../lib/data";
+import Head from "next/head";
 
 export default function Movies() {
   return (
     <>
+      <Head>
+        <title>List of Movies</title>
+      </Head>
       <h1>List of Movies</h1>
       <ul>
-        <li>
-          <Link href={`/movies/aquaman`}>Aquaman</Link>
-        </li>
+        {movies.map(({ id, slug, title }) => (
+          <li key={id}>
+            <Link href={`/movies/${slug}`}>{title}</Link>
+          </li>
+        ))}
       </ul>
     </>
   );
